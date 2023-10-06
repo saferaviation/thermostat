5b0a1a75bc5f4e7724dcff89f7b918b5806fc1eb
elicohen <eliarchercohen@gmail.com>
2023-10-05 21:38:35 -0400

    remote sensor

--- sensor_control.py
+++ sensor_control.py
@@ -2,6 +2,7 @@ import socket
 import datetime
 import time
 import config
+import glob
 
 
 # Define the remote server's host and port
@@ -13,9 +14,33 @@ ROOM = 'JULIAN'
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
 
 def get_temperature():
-    return 75.0
+    return read_temp()[1]
 
 
 def timestamp():
@@ -48,6 +73,7 @@ def main():
         # Close the socket
         client_socket.close()
         time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
