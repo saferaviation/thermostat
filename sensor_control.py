d00bf525a576f921179e9986e5df65ae628f11e3
elicohen <eliarchercohen@gmail.com>
2023-10-05 21:40:13 -0400

    remote sensor

--- sensor_control.py
+++ sensor_control.py
@@ -63,6 +63,7 @@ def main():
         ts = timestamp()
         pk = ts + ',' + ROOM + ',' + str(temp)
         message = ts + ',' + ROOM + ',' + str(temp) + ',F,' + pk
+        print(message)
 
         client_socket.sendall(message.encode('utf-8'))
 
